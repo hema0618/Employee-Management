53bded165f804207d59d1dc643320d8640d3346c
Hema <khemalatha507@gmail.com>
2020-08-23 12:53:36 -0400

    working seed.sql,schema.sql

--- seed.sql
+++ seed.sql
@@ -9,7 +9,7 @@ VALUES
     ("IT");
 
     INSERT INTO role
-        (title,salary,department_id)
+        (title,salary, department_id)
     VALUES
         ("Sales person", 100000, 1),
         ("Mechanical Engineer",120000, 2),
@@ -17,15 +17,15 @@ VALUES
         ("Junior Engineer",85000, 2),
         ("HR Drone", 80000, 3),
         ("IT HERO", 70000, 4);
-
+        
         INSERT INTO employee
-            (first_name, last_name,role_id,manager_id)
+            (first_name, last_name, role_id, manager_id)
         VALUES
-            ("Erin", "platvoet", 1, 20),
-            ("Manoli","IDK", 2, 100),
+            ("Erin", "platvoet", 1, 1),
+            ("Manoli","IDK", 2, 2),
             ("Hojin", "H", 2, NULL),
             ("Thomas", "chalamee", 2, NULL),
-            ("Kyle", "Andie", 3 ,33),
-            ("Ed", "Gibbons", 4, 73);
+            ("Kyle", "Andie", 3 ,3),
+            ("Ed", "Gibbons", 4, 4);
         
     

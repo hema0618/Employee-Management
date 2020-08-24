33dafc04fe00e24d74ff736d970f543a792ab3a3
Hema <khemalatha507@gmail.com>
2020-08-24 17:13:09 -0400

    created a function to start

--- index.js
+++ index.js
@@ -22,5 +22,49 @@ var connection = mysql.createConnection({
 
   connection.connect(function(err) {
     if (err) throw err;
-    runSearch();
+    startApp();
   });
+
+  //creating a function
+
+  function startApp() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View all employees",
+            "View all departments",
+            "View all roles",
+            "Add an employee",
+            "Add department",
+            "Add a role",
+            "EXIT"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "View all employees":
+          viewEmployees();
+          break;
+          case "View all roles":
+                viewRoles();
+                break;
+            case "Add an employee":
+                addEmployee();
+                break;
+            case "Add department":
+                addDepartment();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "EXIT": 
+                endApp();
+                break;
+  
+       
+        }
+      });
+  }
